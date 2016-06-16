92768b8c347d17b7944cda9d08684927cddf4cfd
Jonathan E. Chen <code@chen.io>
2016-06-16 13:37:36 -0700

    Initial Commit

---
+++ ProtoTinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtoTinder
+//
+//  Created by Jonathan Chen on 6/16/16.
+//  Copyright © 2016 Chenlo Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
